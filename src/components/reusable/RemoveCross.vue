0d1afa7813d44a46f3a4885a87b4ef18c014515c
Smidra <rad.smid@gmail.com>
2022-11-20 16:04:59 +0100

    Comments cleanup.

--- src/components/reusable/RemoveCross.vue
+++ src/components/reusable/RemoveCross.vue
@@ -3,9 +3,6 @@
 
 <template>
   <div class="outer-div flex justify-end">
-<!--     <div @click="$emit('click')" class="inner-div">
-      ✖️
-    </div> -->
     <button class="btn btn-circle btn-outline btn-error">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
