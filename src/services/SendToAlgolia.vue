9432964f89cfea9e0e0f3e418b15aab2bb865e29
Smidra <rad.smid@gmail.com>
2022-12-01 11:40:03 +0100

    Send makes dialogs based on succes state.

--- src/services/SendToAlgolia.vue
+++ src/services/SendToAlgolia.vue
@@ -1,31 +1,44 @@
 <script setup>
 import algoliasearch from "algoliasearch"
+import { useDialog } from "naive-ui";
 
 const { jsonToSend } = defineProps(['jsonToSend'])
 
-function submitSuccess(response) {
-  console.log(response);
-  location.reload()
-}
-
-function submitFail(error) {
-  console.log(error);
-}
-
+/* Submit the whole JSON to algolia */
 function submitHandler() {
-  console.log("Hello from submit handler.");
-
   const client = algoliasearch('S27OT8U78J', '39e5cf3041647ce2f68c09b8e477eb8c') // API Key to EDIT
   const index = client.initIndex('firmy')
   const savePromise = index.saveObject(jsonToSend)
-  savePromise.then(submitSuccess)
-  savePromise.catch(submitFail)
+  savePromise.then(handleSuccess)
+  savePromise.catch(handleFailure)
 }
 
+/* Dialogs to display based on how the promise finished. Did it send OK? */
+const dialog = useDialog();
+function handleSuccess() {
+  dialog.success({
+    title: "Úspěch",
+    content: "Firma byla úspěšně vložena do databáze.",
+    negativeText: "Úžas!",
+    onNegativeClick: () => {
+      location.reload()
+    }
+  });
+}
+function handleFailure() {
+  dialog.error({
+    title: "Neúspěch",
+    content: "Něco se stalo, a firma nebyla přidána do databáze.",
+    negativeText: "Dobrá tedy",
+    onNegativeClick: () => {
+      console.log("Bad!");
+    }
+  });
+}
 </script>
 
 <template>
-  <div class="mb-60">
+  <div class="">
     <n-button size="large" @click="submitHandler()"> Přidat do databáze </n-button>
   </div>
 </template>
